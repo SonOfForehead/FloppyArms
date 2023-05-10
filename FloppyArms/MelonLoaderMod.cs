6c527701d3a45e434ee94b609def8866ecfe104c
Son of Forehead <jpstoner@icloud.com>
2023-05-10 09:47:40 -0600

    Add project files.

---
+++ FloppyArms/MelonLoaderMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+using SLZ.Rig;
+using BoneLib.BoneMenu;
+using BoneLib.BoneMenu.Elements;
+using BoneLib;
+
+namespace FloppyArms
+{
+    public static class BuildInfo
+    {
+        public const string Name = "FloppyArms"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Son of Forehead"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class FloppyArms : MelonMod
+    {
+
+        private MelonPreferences_Category FloppyArmsCategory;
+        private MelonPreferences_Entry<bool> MogToggle;
+
+        public override void OnInitializeMelon()
+        {
+            FloppyArmsCategory = MelonPreferences.CreateCategory("Floppy Arms");
+            MogToggle = FloppyArmsCategory.CreateEntry<bool>("Mog Toggle", true);
+            FloppyArms.CreateBoneMenu();
+        }
+
+        public static void CreateBoneMenu()
+        {
+            MenuCategory menuCategory = MenuManager.CreateCategory("Floppy Arms", Color.blue);
+            menuCategory.CreateBoolElement("Mod Toggle", Color.magenta, FloppyArms.IsEnabled, new Action<bool>(FloppyArms.));
+        }
+        public static bool IsEnabled { get; private set; }
+
+        public static bool OnSetEnabled { get; private set; }
+
+    }
+}
---
+++ FloppyArms/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(FloppyArms.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(FloppyArms.BuildInfo.Company)]
+[assembly: AssemblyProduct(FloppyArms.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + FloppyArms.BuildInfo.Author)]
+[assembly: AssemblyTrademark(FloppyArms.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(FloppyArms.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(FloppyArms.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(FloppyArms.FloppyArms), FloppyArms.BuildInfo.Name, FloppyArms.BuildInfo.Version, FloppyArms.BuildInfo.Author, FloppyArms.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
